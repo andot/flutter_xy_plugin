57ca87f98639b1d17d5a8d74ff16931ddb17d744
andot <mabingyao@gmail.com>
2020-01-09 14:01:53 +0800

    Added some fake properties for Android on iOS.
    Added oaid property.
    Added landingPageDisplayActionBarEnabled property.
    Added landingPageAnimationEnabled property.
    Added landingPageFullScreenEnabled property.

--- ios/Classes/FlutterXyPlugin.m
+++ ios/Classes/FlutterXyPlugin.m
@@ -1,20 +1,64 @@
+@import AdtalosSDK;
+
 #import "FlutterXyPlugin.h"
 
-@implementation FlutterXyPlugin
+@implementation FlutterXyPlugin {
+    NSNumber* _landingPageDisplayActionBarEnabled;
+    NSNumber* _landingPageAnimationEnabled;
+    NSNumber* _landingPageFullScreenEnabled;
+}
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"flutter_xy_plugin"
-            binaryMessenger:[registrar messenger]];
-  FlutterXyPlugin* instance = [[FlutterXyPlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"flutter_xy_plugin"
+                                     binaryMessenger:[registrar messenger]];
+    FlutterXyPlugin* instance = [[FlutterXyPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (instancetype)init {
+    if (self = [super init]) {
+        _landingPageDisplayActionBarEnabled = [NSNumber numberWithBool:NO];
+        _landingPageAnimationEnabled = [NSNumber numberWithBool:NO];
+        _landingPageFullScreenEnabled = [NSNumber numberWithBool:NO];
+    }
+    return self;
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
+    if ([call.method isEqualToString:@"setOAID"]) {
+        result(nil);
+        return;
+    }
+    if ([call.method isEqualToString:@"isLandingPageDisplayActionBarEnabled"]) {
+        result(_landingPageDisplayActionBarEnabled);
+        return;
+    }
+    if ([call.method isEqualToString:@"isLandingPageAnimationEnabled"]) {
+        result(_landingPageAnimationEnabled);
+        return;
+    }
+    if ([call.method isEqualToString:@"isLandingPageFullScreenEnabled"]) {
+        result(_landingPageFullScreenEnabled);
+        return;
+    }
+    if ([call.method isEqualToString:@"setLandingPageDisplayActionBarEnabled"]) {
+        _landingPageDisplayActionBarEnabled = call.arguments[@"enabled"];
+        result(nil);
+        return;
+    }
+    if ([call.method isEqualToString:@"setLandingPageAnimationEnabled"]) {
+        _landingPageAnimationEnabled = call.arguments[@"enabled"];
+        result(nil);
+        return;
+    }
+    if ([call.method isEqualToString:@"setLandingPageFullScreenEnabled"]) {
+        _landingPageFullScreenEnabled = call.arguments[@"enabled"];
+        result(nil);
+        return;
+    }
+    
     result(FlutterMethodNotImplemented);
-  }
 }
 
 @end
